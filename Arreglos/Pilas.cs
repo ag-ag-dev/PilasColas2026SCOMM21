ca0483f0d916f66169138767233e4b160ded67a3
Saigo <adrielitzprox220@gmail.com>
2026-03-21 14:02:55 -0700

    - Implementando metodo de agregar

---
+++ Arreglos/Pilas.cs
@@ -0,0 +1,26 @@
+﻿namespace Arreglos
+{
+    internal class Pilas
+    {
+        private string[] _arreglo;
+        private int _tope;
+        public Pilas(int elementos)
+        {
+            _arreglo = new string[elementos];
+            _tope = 0;
+        }
+
+        public void Agregar(string elemento)
+        {
+            int max = _arreglo.Length;
+
+            if (_tope == max)
+            {
+                throw new Exception("Pila llena");
+            }
+
+            _arreglo[_tope] = elemento;
+            _tope++;
+        }
+    }
+}
