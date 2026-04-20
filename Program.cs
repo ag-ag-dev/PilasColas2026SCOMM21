5af74dafb65738a0fef6b7656009f10f1db68130
Adriel G. <211076902+ag-ag-dev@users.noreply.github.com>
2026-04-19 17:37:19 -0700

    - Multiples cambios
    - Se hizo el código de una forma más estética
    - Se agrego el método de "ColasCirc" a "Program"

--- Program.cs
+++ Program.cs
@@ -4,10 +4,14 @@
     {
         static void Main(string[] args)
         {
+            // Usamos el try para mostrar algun error que pueda pasar al ejecutar Pilas
             try
             {
+                // Creamos el objeto de Pilas, para posteriormente asignarle 5 objetos, es decir, podemos agregar hasta 5 datos en el array de Pilas.
+                // Posteriormente ponemos a prueba el funcionamiento de Pilas, se agregan datos, se eliminan, y se obtienen los datos y se muestran en consola
                 Pilas pila = new Pilas(5);
 
+                Console.WriteLine("Metodo Pilas");
                 pila.Agregar("A");
                 pila.Agregar("B");
                 pila.Agregar("C");
@@ -30,13 +34,19 @@
                 pila.Eliminar();
                 Console.WriteLine(pila.ObtenerDatos());
             }
+            // Aqui terminamos con el uso de try y usamos catch para que el programa agarre cualquier error que nos
+            // pueda presentar en la ejecucion del codigo, y posteriormente nos muestre el error general no especificado
             catch (Exception ex)
             {
                 Console.WriteLine(ex.Message);
 
+                // -------------------------------- COLAS ---------------------------------
+
+                // Se repite el uso de try y catch para capturar algun error del programa
                 try
                 {
-                    // Impresiones en consola del metodo Colas
+                    // Mas impresiones en consola pero ahora sobre el metodo Colas:
+                    // Se crea el array cola, se le asignan los 5 datos, se agregan, se eliminan, y se obtienen los datos y se muestran en consola
                     Console.WriteLine("");
                     Console.WriteLine("-------------------------------------------------------------------------");
                     Console.WriteLine("");
@@ -64,12 +74,50 @@
                     Console.WriteLine("Eliminando C");
                     cola.Eliminar();
                     Console.WriteLine(cola.ObtenerDatos());
+
+                    cola.Agregar("A");
+                    Console.WriteLine("Agregando A");
+                    Console.WriteLine(cola.ObtenerDatos());
                 }
 
                 // Se usa Catch para mostrar algun error que se pueda presentar en el metodo Colas
                 catch (Exception ex2)
                 {
                     Console.WriteLine(ex2.Message);
+                }
+
+                // -------------------------------- COLAS CIRCULARES ---------------------------------
+
+                // Mas de lo mismo, usamos try, catch y ex para capturar errores y enseñarlos en la ejecucion
+                {
+                    try
+                    {
+                        // El mismo proceso, se crea el array de colas circulares [esta vez no se especifica el tamaño del array aqui, si no en el archivo class de ColasCirc, que es 4]
+                        // Se agregan datos, se restan, se desplanzan los datos y finalmente se enseñan en consola
+                        Console.WriteLine("");
+                        Console.WriteLine("-------------------------------------------------------------------------");
+                        Console.WriteLine("");
+                        Console.WriteLine("Metodo Colas circulares");
+
+                        ColasCirc cola2 = new ColasCirc();
+                        cola2.Agregar("A");
+                        cola2.Agregar("B");
+                        cola2.Agregar("C");
+                        cola2.Agregar("D");
+                        cola2.MostrarDatos();
+
+                        cola2.Agregar("E");
+                        cola2.Agregar("F");
+                        cola2.Agregar("G");
+                        cola2.MostrarDatos();
+
+                        cola2.Eliminar();
+                        cola2.MostrarDatos();
+                    }
+                    catch (Exception ex3)
+                    {
+                        Console.WriteLine(ex3.Message);
+                    }
                 }
             }
         }
